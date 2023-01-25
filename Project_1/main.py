e399456503c0fbc1a0304fbf419dacd09a3d5e00
Peter <peter.bboy.master@gmail.com>
2023-01-25 20:25:53 +0100

    init:Initial push

---
+++ Project_1/main.py
@@ -0,0 +1,8 @@
+#Name Band generator
+
+city =input("Introduce your city name: ")
+print("Ok I see you city is: ", city)
+pet_name = input("Introduce your pets name: ")
+print("Oh, your pets name is: ", pet_name)
+
+print("Well then I think your band name should be ",city, pet_name)
